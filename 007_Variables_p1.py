8720ba33cf8cb9cd052b7b73fb3447d1159643af
Assem Ayman <assemayman136@gmail.com>
2023-10-25 22:11:04 +0300

    day 2

---
+++ 007_Variables_p1.py
@@ -0,0 +1,12 @@
+#_________________________________________
+#__________ Variables ____________________
+# Syntax =>  [Variable Name] = [variable Value]
+# Name Conventions and Rules:
+# Can Start with (a-x A-Z _)
+# Can not start with (1-9 or special characters)
+# Can Include numbers and _
+# Can not include special characters
+# Name is case sensitive
+
+my_variable = 124   # snake_case is best practice
+print(my_variable)
---
+++ 008_Variables_p2.py
@@ -0,0 +1,17 @@
+#____________________________________
+#_____________Variables______________
+#____________________________________
+# Sourrce code : Original Code You write it in computer.
+# Translation : Converting source code to Machine Code.
+# Compilation : Translating Code before runtime
+# Run-time : Period App takes to Executing commans
+# Interpreted :code translated on the fly during Excution
+# python is dynamically typed language : can change during runtime
+#____________________________________
+
+x =10 
+x ="hello world"
+print(x);
+
+
+
