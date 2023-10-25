2a2fec55e3c174199ddfbbc7b740d10c6b310d7d
Assem Ayman <assemayman136@gmail.com>
2023-10-25 22:55:29 +0300

    day 2

--- 010_Concatenation_And_Training.py
+++ 010_Concatenation_And_Training.py
@@ -13,5 +13,11 @@ Second \
 Third"      # Frist Second Third
 b = " A \
 B \
-C"         #Frist Second Third A B C
-print(a+b)
+C"     
+print(a+b)    #Frist Second Third A B C
+c = a + "\n" + b 
+print(c) #Frist Second Third
+         # A B C 
+
+#print("one"+ 1) #error: can only concatenate str to str
+print("one",1) # one 1
