bc0d04af6bd11b02f0749b1919255fd69c2b86ac
Assem Ayman <assemayman136@gmail.com>
2023-10-26 14:12:02 +0300

    day 3

--- 014 _Strings_Methods_P_2.py
+++ 014 _Strings_Methods_P_2.py
@@ -7,3 +7,10 @@ print(a.split()) # split by spaces and as elements of list
 
 b = "I-love-python-and-php"
 print(b.split(sep='-',maxsplit=2)) # split by spaces and as elements of
+
+c = "I-love-python-and-php"
+print(c.rsplit('-',maxsplit=2)) # split by
+
+e = "Osama"
+print(e.center(11,'*'))
+
