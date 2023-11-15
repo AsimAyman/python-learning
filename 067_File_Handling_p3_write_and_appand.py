80442c05c8110b8194153950e8b20eb01bc3043e
Assem Ayman <assemayman136@gmail.com>
2023-11-15 22:19:33 +0200

    file

--- 067_File_Handling_p3_write_and_appand.py
+++ 067_File_Handling_p3_write_and_appand.py
@@ -2,7 +2,16 @@
 #----File handling => Write and append in file---
 #------------------------------------------------
 
-myFile = open('file.txt', 'w') # it will create the file
+# myFile = open('file.txt', 'w') # it will create the file
 # myFile.write('Hello from python file with love \n')
 # myFile.write('Second line') # 
-myFile.write('therd part') #evry time it will remove the current txt and write the new write
+# myFile.write('therd part') #evry time it will remove the current txt and write the new write
+# myFile.write('assem school\n'  * 1000)
+
+
+# myFile.writelines(['hello\n','there\n','i\n','am\n','assem\n'])
+
+
+#append
+myFile = open('file.txt', 'a')
+myFile.write('new line') # will not remove the prevese txt
