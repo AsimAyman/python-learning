9256c47847ac10bf2c12e4f84c4d7b0e7539402e
Assem Ayman <assemayman136@gmail.com>
2023-11-17 13:39:26 +0200

    buit 3

---
+++ 073_built_in_function_p5.py
@@ -0,0 +1,26 @@
+# --------------------------------
+# ----Built in function-----------
+# --------------------------------
+# filter take a function + iterator
+# filter run a function on every element
+# the function can be predefined functio or lambda funciton 
+# filter out all elements for which the function return true
+# the function need to retruen boolean value
+#---------------------------------
+
+def checkNumber(n):
+    return True if n >10 else False
+a = [1,2,33,14,50,6,7]
+b = filter(checkNumber,a)
+print(a)  #[1, 2, 33, 14, 50, 6, 7]
+print(list(b)) #[33, 14, 50]
+print('-'*40)
+
+
+def checkName(name):
+    return name.startswith('O')
+
+names = ['Osama','Omar','othman','adek']
+o_names = filter(checkName,names)
+print(list(o_names))
+print(list(filter(lambda name:name.startswith('O'),names)))
