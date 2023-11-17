431b06a1fb4ddbb3f5f8e4ff587651d54e2c2939
Assem Ayman <assemayman136@gmail.com>
2023-11-17 13:25:37 +0200

    buit 3

--- 072_built_in_fuction_p4.py
+++ 072_built_in_fuction_p4.py
@@ -1,3 +1,31 @@
 # --------------------------------
 # ----Built in function-----------
 # --------------------------------
+# map take A functoin + iterator
+# map called map because it map the function on every element
+# the function can be pre-defined function or lambda function
+#---------------------------------
+
+# use map with predefined function
+
+def formatText(text):
+    return f"-{text.strip().capitalize()}-"
+
+
+my_text = {'assem','Ahmed','sayed'}
+
+my_text_formated = map(formatText,my_text)
+
+for name in my_text_formated:    
+    print(name)
+
+for name in map(formatText,my_text):  
+    print(name)
+
+my_text_formated = list(map(formatText,my_text))    
+
+
+# use map with lambda function
+my_text = {'assem','Ahmed','sayed'}
+
+my_text_formated = map(lambda t  : f"-{t.strip().capitalize()}-"  ,my_text)
