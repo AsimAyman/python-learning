e3f9f0cefb7c1087f14dbe20b4ff6c48001c8255
Assem Ayman <assemayman136@gmail.com>
2023-11-29 20:13:55 +0200

    decorator

---
+++ 084_decorator_function_with_parameters.py
@@ -0,0 +1,27 @@
+#--------------------------------------------
+#-----Decorator => Function with Parameters--
+#--------------------------------------------
+
+def myDecorator (func):
+    def inner(n1,n2):
+        if type(n1) is int and type(n2) is int:
+            if n1 >2 or n2 >2:
+                print('One of the numbers is greater than 2')
+            print("the numbers is int")
+            func(n1,n2)
+
+    
+    return inner;
+
+def myDecorator2(fun):
+    def inner(n1,n2):
+        print('from decorator 2')
+        fun(n1,n2)
+    return inner 
+       
+@myDecorator
+@myDecorator2
+def calc(n1, n2): 
+    print(n1+n2)
+
+calc(1,3)    
