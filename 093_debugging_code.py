0ec5f3de0dc6e8acc02447b8a5eb6f5dd0685155
Assem Ayman <assemayman136@gmail.com>
2023-12-15 18:15:33 +0200

    debugger

--- 093_debugging_code.py
+++ 093_debugging_code.py
@@ -1,3 +1,15 @@
 #-----------------------
 #------Debugging Code---
 #-----------------------
+my_list = [1,2,3]
+my_dictionary = {'Name': 'osama','age':12,'country':'egypt'}
+for num in my_list:
+    print(num)
+
+for key,val in my_dictionary.item():
+    print(key ,val) 
+
+def function_one():
+    print('hello from function one')
+
+function_one()
