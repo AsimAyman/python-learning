fbe12c629c04076f48d99ab9031e6a0a3273f063
Assem Ayman <assemayman136@gmail.com>
2023-12-15 18:06:47 +0200

    add

--- 092_exceptoins_handling_advanced_example.py
+++ 092_exceptoins_handling_advanced_example.py
@@ -4,9 +4,23 @@
 #-------------Advanced Example --------------
 #--------------------------------------------
 
+the_file = None
 tries = 3
 while tries > 0:
-    file_name = input("enter the file name")
-    
-    print(f"you have {tries}")
-    tries -=1    
+    try:
+        file_name = input("enter the file name => ").strip()
+        the_file = file_name.open(the_file,'r')
+        print(the_file.read())
+        break
+    except FileNotFoundError:
+        print(f"you have {tries}")
+        tries -=1    
+    except:
+        print(f"you have {tries}")
+        print('error is found')
+        tries -=1
+    finally:
+        the_file.close()
+        print('final section')    
+else:
+    print('else section')        
