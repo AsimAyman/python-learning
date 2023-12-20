8e3f4ceda6ead6725f8c9fd6660dcd52f41f8523
Assem Ayman <assemayman136@gmail.com>
2023-12-20 20:52:42 +0200

    regular

---
+++ 099_regular_expressions_logical_or_escaping.py
@@ -0,0 +1,8 @@
+#--------------------------------------------------
+#----Regular Expressions=>logical or and escaping--
+#--------------------------------------------------
+# | or
+# \ escape special characters
+# () separate groups
+#---------------------------------
+#^(https?://)(www.)?[A-z]+.((net)|(org)|(com))$
